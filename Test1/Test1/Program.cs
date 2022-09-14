a5264c4d6c88cc7b6b884544edc2f2c5df466f85
지현 <372491@naver.com>
2022-09-14 19:21:49 +0900

    no message

---
+++ Test1/Test1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Test1
+{
+    internal class Program
+    {
+        static void Main(string[]args)
+        {
+            double d = 0d;
+            decimal m = 0m;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                d += 0.10d;
+                m += 0.10m;
+            
+            }
+            Console.WriteLine(d);
+            Console.WriteLine(m);
+        }
+    }
+}
