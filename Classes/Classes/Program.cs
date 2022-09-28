ee36bb73d7c670698069a4599dbf44242a9394c7
지현 <372491@naver.com>
2022-09-28 12:30:02 +0900

    0928 C#프로그래밍 delegate etc.

---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace Classes
+{
+public class Color
+    {
+        public const int FULL = 0xFF;
+        public const int EMPTY = 0x00F;
+
+        private byte red, green, blue;
+        private int fULL;
+        private int eMPTY1;
+        private int eMPTY2;
+        public static readonly Color Red = new Color(FULL, EMPTY, EMPTY);
+        public static readonly Color blue = new Color(EMPTY, EMPTY, FULL);
+        public static readonly Color green = new Color(EMPTY, FULL, EMPTY);
+
+        public Color(byte red, byte blue, byte green)
+        {
+            this.red = red;
+            this.blue = blue;
+            this.green = green;
+        }
+
+        public void PrintColor(Color c)
+        {
+            Console.WriteLine("{0}, {1}, {2}", c.red, c.blue, c.green);
+        }
+    } 
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        { Console.WriteLine(Color.FULL);
+            Color.PrintColor(Color.Red);
+            
+        }
+    }
+}
---
+++ Deligate/Deligate/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Delegate
+{
+    delegate void dele();
+    delegate void d2(int i);
+
+    class AAA
+    {
+        public void M1()
+        {
+            Console.WriteLine("AAA.M1()");
+        }
+        public void M2(int a)
+        {
+            Console.WriteLine("AAA.M2()" + a);
+        }
+        public void M3()
+        {
+            Console.WriteLine("AAA.M3()");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AAA a = new AAA();
+            dele d1 = new dele(a.M1); // m1 이 등록됨
+            d1 += new dele(a.M3);
+            d1(); //델리게이트로 d1을 호출 m3이 있으면 두 개를 한번에 같이 부를 수 있음 (매개변수 모양 같은 애들끼리)
+
+            d2 dd = new d2(a.M2);
+            dd(10);
+        }
+    }
+}
+
