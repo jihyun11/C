b8eb4a299979c516aab8c92853705ceeea201cb3
지현 <372491@naver.com>
2022-10-12 11:39:15 +0900

    1012 C#프로그래밍 제네릭, 조건부 속성

---
+++ genericMethod/genericMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace GenericMethod
+{
+    class Program
+    {
+        static void Swap<T>(ref T x, ref T y)
+        {
+            T temp = x;
+            temp = x;
+            x = y;
+            y = temp;
+        }
+    } static void Main(string[] args)
+    {
+        int a = 1, b = 2; double c = 1.5, double d = 2.5;
+        Program p = new Program();
+        
+        Swap<int>(ref a, ref b);
+        Console.WriteLine("{0}, {1}", a, b);
+    }
+}
+
