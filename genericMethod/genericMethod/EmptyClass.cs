51bdb45456145705389119f1e68c717cd64a216a
지현 <372491@naver.com>
2022-10-14 13:26:20 +0900

    C#프로그래밍 인터페이스로 업캐스팅

---
+++ genericMethod/genericMethod/EmptyClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace genericMethod
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+
+        {
+            var sheeps = new bool[] { true, false, true };
+            int result = 0;
+            for (int i = 0; i < sheeps.Length; i++)
+            {
+                if (sheeps[i] == true)
+                {
+                    result++;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+    }
+
+
---
+++ genericMethod/genericMethod/throw.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace genericMethod
+{
+    class Program
+	{
+		static void throw(int age)
+		{
+			if (age< 10) {
+				Console.WriteLine("age: {0}", age);
+		} else throw new Exception("age가 10보다 큽니다.");
+	}
+} static void Main(string[] args)
+{
+	try
+    {
+		throw (1);
+		throw (10);
+    }
+	catch (Exception e)
+    {
+		Console.WriteLine(e.Message);
+    }
+}
+
